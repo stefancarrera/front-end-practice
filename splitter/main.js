f53980c20050708e32717f25803d711cd8555fe3
Stefan Carrera <stefancarrera@gmail.com>
2021-08-31 01:46:50 -0500

    Custom tip amt added
    Custom tip amt working

--- splitter/main.js
+++ splitter/main.js
@@ -4,9 +4,28 @@ const $tipAmtPer = document.getElementById('tipAmtPer');
 const $totalAmtPer = document.getElementById('totalAmtPer');
 const $numPeople = document.getElementById('numPeople');
 const $reset = document.getElementById('reset');
+const $custom = document.getElementById('custom');
 
 $tipRow.addEventListener('click', function (event) {
   if (parseInt($bill.value) <= 0) {
+    $tipAmtPer.textContent = 'Bill Amount Required';
+  } else {
+    if ($numPeople.value <= 1) {
+      const tipAmt = parseInt($bill.value * `0.${event.target.value}`);
+      $tipAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(tipAmt);
+      const totalAmt = parseInt($bill.value) + parseInt(tipAmt);
+      $totalAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(totalAmt);
+    } else {
+      const tipAmt = parseInt($bill.value * `0.${event.target.value}`);
+      $tipAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(tipAmt / $numPeople.value);
+      const totalAmt = parseInt($bill.value) + parseInt(tipAmt);
+      $totalAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(totalAmt / $numPeople.value);
+    }
+  }
+});
+
+$custom.addEventListener('change', function (event) {
+  if ((parseInt($bill.value) <= 0) && (Number.isInteger(parseInt(event.target.value)))) {
     $tipAmtPer.textContent = 'Bill Amount Required';
   } else {
     if ($numPeople.value <= 1) {
@@ -28,4 +47,5 @@ $reset.addEventListener('click', function (event) {
   $totalAmtPer.textContent = '$0.00';
   $bill.value = '0.00';
   $numPeople.value = '1';
+  $custom.value = 'Custom';
 });
