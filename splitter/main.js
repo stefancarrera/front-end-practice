5d0feba8effe60311d59d38c662238656087e023
Stefan Carrera <stefancarrera@gmail.com>
2021-08-31 01:17:27 -0500

    Total amt per person displaying
    Total amt including tip and bill displaying for each tip amount clicked

--- splitter/main.js
+++ splitter/main.js
@@ -1,13 +1,20 @@
 const $bill = document.getElementById('bill');
 const $tipRow = document.getElementById('tipRow');
 const $tipAmtPer = document.getElementById('tipAmtPer');
-// const $totalAmtPer = document.getElementById('totalAmtPer');
+const $totalAmtPer = document.getElementById('totalAmtPer');
+const $numPeople = document.getElementById('numPeople');
 
 $tipRow.addEventListener('click', function (event) {
   if (parseInt($bill.value) <= 0) {
     $tipAmtPer.textContent = 'Bill Amount Required';
   } else {
-    $tipAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format($bill.value * `0.${event.target.value}`);
-
+    if ($numPeople.value <= 1) {
+      const tipAmt = parseInt($bill.value * `0.${event.target.value}`);
+      $tipAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(tipAmt);
+      console.log('tip', tipAmt);
+      const totalAmt = parseInt($bill.value) + parseInt(tipAmt);
+      console.log('total', totalAmt);
+      $totalAmtPer.textContent = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(totalAmt);
+    }
   }
 });
